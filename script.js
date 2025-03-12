b8614f46c592a88fa21ba105b08713a5b2cf9cf9
deylinqff <maycraftminecraft4@gmail.com>
2025-03-12 00:07:35 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function toggleMenu() {
+function toggleSideMenu() {
     const sideMenu = document.getElementById('sideMenu');
+    const menuIcon = document.getElementById('menu-icon');
+    
     const currentLeft = sideMenu.style.left;
     
     if (currentLeft === '0px') {
-        sideMenu.style.left = '-250px';  // Oculta el menú
+        sideMenu.style.left = '-250px';  // Ocultar el menú
+        menuIcon.innerHTML = '&#9776;';  // Volver al ícono de menú
     } else {
-        sideMenu.style.left = '0px';  // Muestra el menú
+        sideMenu.style.left = '0px';  // Mostrar el menú
+        menuIcon.innerHTML = '&#10006;';  // Cambiar al ícono de "X"
     }
 }
