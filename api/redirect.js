6e4416aee244f2ab9b586755f193ddbfafa2abd7
deylinqff <maycraftminecraft4@gmail.com>
2025-03-14 11:41:39 -0600

    Actualizar redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -5,8 +5,6 @@ module.exports = (req, res) => {
         return res.status(400).json({ success: false, message: 'No se proporcionó URL de la imagen.' });
     }
 
-    // Generar la URL personalizada con tu dominio
-    const customUrl = `https://kirito-md.vercel.app/imagen/${encodeURIComponent(url)}`;
-
-    return res.status(200).json({ success: true, customUrl });
+    // Redirige al enlace de ImgBB directamente
+    return res.redirect(url);
 };
