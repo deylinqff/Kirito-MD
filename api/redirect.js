751d472598f7d761d691da04b0e5ed1ee2fd9f29
deylinqff <maycraftminecraft4@gmail.com>
2025-03-14 11:48:01 -0600

    Actualizar redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,10 +1,13 @@
 module.exports = (req, res) => {
-    const { url } = req.body;
+    const { url } = req.query;
 
     if (!url) {
         return res.status(400).json({ success: false, message: 'No se proporcionó URL de la imagen.' });
     }
 
-    // Redirige al enlace de ImgBB directamente
-    return res.redirect(url);
+    // Decodificar la URL codificada
+    const decodedUrl = decodeURIComponent(url);
+
+    // Redirigir a la URL de ImgBB
+    return res.redirect(decodedUrl);
 };
