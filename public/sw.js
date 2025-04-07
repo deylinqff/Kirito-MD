4e06963aac1e2c7534e2355f0c13d4ab46a6e10b
deylinqff <maycraftminecraft4@gmail.com>
2025-04-06 19:36:50 -0600

    Actualizar sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-self.addEventListener("install", (event) => {
+//self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("kirito-cache-v1").then((cache) => {
       return cache.addAll([
