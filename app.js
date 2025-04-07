96dbddebdefd7d729df97fc7f0578737acc96626
deylinqff <maycraftminecraft4@gmail.com>
2025-04-06 19:43:24 -0600

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-document.getElementById("clickButton").addEventListener("click", function() {
+/*document.getElementById("clickButton").addEventListener("click", function() {
     alert("¡Haz hecho clic en el botón!");
-});
+});*/
