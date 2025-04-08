d1f735e3b4cc3a318a092c97db9a75076ad48fdf
deylinqff <maycraftminecraft4@gmail.com>
2025-04-07 22:40:51 -0600

    Actualizar ia-img.js

--- ia-img.js
+++ ia-img.js
@@ -1,17 +1,4 @@
-const IMAGE_API_URL = "https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=";
-
-async function generarImagen(prompt) {
-    try {
-        const response = await fetch(IMAGE_API_URL + encodeURIComponent(prompt));
-        const imageData = await response.json();
-        return imageData.url || "No se pudo generar la imagen.";
-    } catch (error) {
-        console.error("Error al generar imagen:", error);
-        return "Hubo un error al generar la imagen.";
-    }
-}
-
-export { generarImagen };
+const IMAGE_API_URL = "https://api.agungny.my.id/api/text2img?prompt=";
 
 async function generarImagen(prompt) {
     try {
